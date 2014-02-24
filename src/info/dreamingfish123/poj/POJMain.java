452c1f1aec6781441272288ba3e3e3780c183efe
abysshal <abysshal@gmail.com>
2014-02-24 17:54:43 +0800

    init..
    use j2se1.5

---
+++ src/info/dreamingfish123/poj/POJMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package info.dreamingfish123.poj;
+
+/**
+ * @author hui
+ *
+ */
+public class POJMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
